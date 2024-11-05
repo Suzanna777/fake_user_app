f5fdcf029c914e4c793247ec85ef8faf2244b623
“Suzanna” <americandreamdoor@gmail.com>
2024-11-05 15:39:44 -0800

    fake_user_app final

--- src/main/java/com/bank/fake_user_app/service/FakeUserService.java
+++ src/main/java/com/bank/fake_user_app/service/FakeUserService.java
@@ -3,5 +3,6 @@ package com.bank.fake_user_app.service;
 public interface FakeUserService {
 
     String getRandomName();
+    String getNameFromDB();
 
 }
--- src/main/java/com/bank/fake_user_app/service/impl/FakeUserServiceImpl.java
+++ src/main/java/com/bank/fake_user_app/service/impl/FakeUserServiceImpl.java
@@ -1,21 +1,32 @@
 package com.bank.fake_user_app.service.impl;
 
+import com.bank.fake_user_app.repository.FakeUserRepository;
 import com.bank.fake_user_app.service.FakeUserService;
 import com.github.javafaker.Faker;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class FakeUserServiceImpl implements FakeUserService {
 
     private final Faker faker;
+    private final FakeUserRepository fakeUserRepository;
 
-    public FakeUserServiceImpl(Faker faker) {
+    public FakeUserServiceImpl(Faker faker, FakeUserRepository fakeUserRepository) {
         this.faker = faker;
+        this.fakeUserRepository = fakeUserRepository;
     }
 
     @Override
     public String getRandomName() {
         return faker.name().fullName();
+    }
+
+    @Override
+    public String getNameFromDB() {
+        Long id = (long) new Random().nextInt(10) + 1;
+        return fakeUserRepository.findById(id).get().getName();
     }
 
 }
